0e431c417a5b78503b11a695e8a84b0a0a9a7b66
rohit <rohit.selvakumaran@gmail.com>
2024-09-14 15:05:49 +0530

    added leave request

--- src/app/leave-request/leave-request.component.ts
+++ src/app/leave-request/leave-request.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-leave-request',
+  standalone: true,
+  imports: [CommonModule, FormsModule], 
   templateUrl: './leave-request.component.html',
   styleUrls: ['./leave-request.component.css']
 })
 export class LeaveRequestComponent {
-  // You can define properties and methods here for the leave request form
-  submitRequest() {
-    alert('Leave request submitted');
+  employeeName: string = '';
+  leaveType: string = '';
+  startDate: string = '';
+  endDate: string = '';
+  noOfDays: number = 0;
+  reason: string = '';
+
+  onSubmit() {
+    console.log({
+      employeeName: this.employeeName,
+      leaveType: this.leaveType,
+      startDate: this.startDate,
+      endDate: this.endDate,
+      noOfDays: this.noOfDays,
+      reason: this.reason
+    });
+  }
+
+  calculateDays() {
+    if (this.startDate && this.endDate) {
+      const start = new Date(this.startDate);
+      const end = new Date(this.endDate);
+      if (end >= start) {
+        const timeDiff = Math.abs(end.getTime() - start.getTime());
+        this.noOfDays = Math.ceil(timeDiff / (1000 * 60 * 60 * 24)) + 1;
+      } else {
+        this.noOfDays = 0;
+      }
+    }
   }
 }
